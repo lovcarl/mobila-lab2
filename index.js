4eef006f64bbfcc49aee50fcc5aa2b1eec839961
lovcarl <lovcarl@users.noreply.github.com>
2019-04-08 14:13:01 +0200

    nu har vi en aboutpage

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ document.addEventListener('init', function(event) {
     page.querySelector('#push-button').onclick = function() {
       document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
     };
+    page.querySelector('#about').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'About'}});
+    };
   } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+  else if (page.id == 'about') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
 });

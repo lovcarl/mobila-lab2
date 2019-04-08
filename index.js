26ec4939a56a88640499c1fbf9025b7dbdd0f2ba
lovcarl <lovcarl@users.noreply.github.com>
2019-04-08 14:33:51 +0200

    jag har fixat ett skelett

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ document.addEventListener('init', function(event) {
   var page = event.target;
 
   if (page.id === 'startpage') {
-    page.querySelector('#push-button').onclick = function() {
+    page.querySelector('#push-button1').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page1.html', {data: {title: 'Page 1'}});
+    };
+    page.querySelector('#push-button2').onclick = function() {
       document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    };
+    page.querySelector('#push-button3').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
     };
     page.querySelector('#about').onclick = function() {
       document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'About'}});

4ae2d608c1c3ed337f606aec44acc6ccaf393db8
lovcarl <lovcarl@users.noreply.github.com>
2019-04-08 14:39:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,18 @@ document.addEventListener('init', function(event) {
     page.querySelector('#about').onclick = function() {
       document.querySelector('#myNavigator').pushPage('about.html', {data: {title: 'About'}});
     };
-  } else if (page.id === 'page2') {
+  } else if (page.id === 'page1') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
+
+  else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+  
+  else if (page.id === 'page3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+
   else if (page.id == 'about') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
